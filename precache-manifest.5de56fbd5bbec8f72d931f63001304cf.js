1bb6c91e59bff973d041f761a72806edeaad9677
Farai Mrape <faraimrape@gmail.com>
2020-04-19 23:21:06 +0200

    Updates

---
+++ precache-manifest.5de56fbd5bbec8f72d931f63001304cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab5ca47175acfc4cf214a51118bdc3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc80c4e1991eb6abf937",
+    "url": "/monsters-rolodex/static/css/main.4184a31d.chunk.css"
+  },
+  {
+    "revision": "98417cbaa538cdc99487",
+    "url": "/monsters-rolodex/static/js/2.009c4aaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.009c4aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc80c4e1991eb6abf937",
+    "url": "/monsters-rolodex/static/js/main.bf55fded.chunk.js"
+  },
+  {
+    "revision": "17ced733544526c8048d",
+    "url": "/monsters-rolodex/static/js/runtime-main.769a4b75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
